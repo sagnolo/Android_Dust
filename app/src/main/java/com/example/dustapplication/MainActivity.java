60ff82255089e41480707f993f16756c4ccc051a
sagnolo <rladnjs401@gmail.com>
2019-02-03 17:54:44 +0900

    Add authorization for gps

--- app/src/main/java/com/example/dustapplication/MainActivity.java
+++ app/src/main/java/com/example/dustapplication/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.dustapplication;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.nfc.Tag;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -39,14 +51,37 @@ import retrofit2.http.Path;
 
 public class MainActivity extends AppCompatActivity {
     private Retrofit retrofit;
-    private TextView textView;
-
+    private TextView textView, gpsData;
+    //int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+    private final int MY_PERMISSIONS_REQUEST_LOCATION=1001;
+    ToggleButton gps;
     private final String BASE_URL = "http://openapi.airkorea.or.kr/";
     private final String Key = "8DIGt1JffYRo9AxFNnQBjfud5kuDiROVhl0CBRCaWS8OJSZqwqH0A4dl2j3lWC+hMLJhn2maHKpIIoivntVtow==";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gps = (ToggleButton) findViewById(R.id.gps);
+        gpsData = (TextView)findViewById(R.id.gpsData);
+
+        // LocationManager 객체 얻어오기
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this,"위치 권한이 필요합니다.",Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+                Toast.makeText(this,"위치 권한이 필요합니다.",Toast.LENGTH_LONG).show();
+            }
+        }
         init();
 
         GitHub gitHub = retrofit.create(GitHub.class);
@@ -59,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         params.put("ServiceKey",Key);
         params.put("_returnType","json");
 
-        //Call<List_Data> call = gitHub.contributors("서울", "DAILY", "1","25", "json", Key);
+        //Call<List_Data> call = gitHub.contributors("서울", "DAILY", "1","25", "json", Key); // 쿼리 하나하나 보낼 때
         Call<List_Data> call = gitHub.contributors(params);
 
         call.enqueue(new Callback<List_Data>(){
@@ -81,11 +116,79 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+
+        gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    if(gps.isChecked()){
+                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                100, // 최소 시간간격 (miliSecond)
+                                1, // 최소 변경거리 (m)
+                                mLocationListener);
+                        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                                100,
+                                1,
+                                mLocationListener);
+                    } else {
+                        lm.removeUpdates(mLocationListener); // 자원 해제
+                    }
+                } catch (SecurityException ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged (Location location) {
+            // 위치값 갱신 -> 이벤트 발생
+            Log.d("test", "onLocationChanged, location:" + location);
+
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude(); //위도
+            double altitude = location.getAltitude(); //고도
+            float accuracy = location.getAccuracy(); //정확도
+            String provider = location.getProvider(); //위치제공자
+            gpsData.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
+                    + "\n고도 : " + altitude + "\n정확도 : "  + accuracy);
+
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+            gpsData.setText("onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,"승인이 허가되어 있습니다.",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this,"아직 승인받지 않았습니다.",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 
     public void init() {
         textView = (TextView)findViewById(R.id.dustData);
-
 
         Gson gson = new GsonBuilder()
                 .setLenient()
